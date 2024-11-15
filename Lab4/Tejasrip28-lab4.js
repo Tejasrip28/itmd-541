596cc266a661e75be07b0284050918184abccd6c
Your Name <tpanchagiri@hawk.iit.edu>
2024-11-15 14:48:49 -0600

    update2

--- Lab4/Tejasrip28-lab4.js
+++ Lab4/Tejasrip28-lab4.js
@@ -10,3 +10,12 @@ include the wrapping quotes):
     })();
 
 
+/*2. Change the line of text below the hero headline to the following text (do not
+include the wrapping quotes, notice the bold and italic):
+“Leverage innovative strategies from Stellar Marketing to make your business
+shine and succeed.”*/
+
+const subHeadline = document.querySelector('#hero p');
+if (subHeadline) {
+subHeadline.innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <strong><em>shine</em></strong> and <strong><em>succeed</em></strong>.";
+}
