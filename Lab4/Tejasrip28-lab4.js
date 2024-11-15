fce4b0f44c4029d2f83ba23c4848f444459bf6ee
Your Name <tpanchagiri@hawk.iit.edu>
2024-11-15 14:49:47 -0600

    update3

--- Lab4/Tejasrip28-lab4.js
+++ Lab4/Tejasrip28-lab4.js
@@ -4,7 +4,6 @@
 include the wrapping quotes):
 “Supercharge Your Brand with Stellar Marketing”*/
 
-
 (function() {
     document.querySelector('#hero h1').textContent = "Supercharge Your Brand with Stellar Marketing";
     })();
@@ -19,3 +18,13 @@ const subHeadline = document.querySelector('#hero p');
 if (subHeadline) {
 subHeadline.innerHTML = "Leverage innovative strategies from Stellar Marketing to make your business <strong><em>shine</em></strong> and <strong><em>succeed</em></strong>.";
 }
+
+
+/*3. Change the image in the background of the hero to the one below:
+https://picsum.photos/id/683/1280/720//*/
+
+const heroSection = document.querySelector('#hero');
+if (heroSection) {
+    heroSection.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720')";
+}
+
